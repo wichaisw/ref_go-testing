84cd893f73ec58e5e0e59669a7a72982b0e7c803
S.Wichai <wichai.s@wisesight.com>
2023-01-02 21:34:23 +0700

    test: test double: fake

---
+++ double/fake_test.go
@@ -0,0 +1,25 @@
+package double
+
+import "testing"
+
+type FakeSearcher struct{}
+
+func (fs FakeSearcher) Search(people []*Person, firstName string, lastName string) *Person {
+	if len(people) == 0 {
+		return nil
+	}
+
+	return people[0]
+}
+
+func TestFindCallsSearchAndReturnsEmptyStringForNoPerson(t *testing.T) {
+	phonebook := &Phonebook{}
+	fake := &FakeSearcher{}
+
+	// phonebook is empty, when call Find, which call fake.Search inside, will return people[0]
+	phone, _ := phonebook.Find(fake, "Jane", "Doe")
+
+	if phone != "" {
+		t.Errorf("Wanted '', got '%s'", phone)
+	}
+}
